b1a71afda4fd9acba82b0b14c481c7c2a82e08a0
Fabio Costa <developerfabiocsilva@gmail.com>
2026-05-28 18:42:36 -0300

    feat: add initial EF Core migration and database setup

---
+++ Backend/EducationalPlataform/Data/EducationalPlataformContext.cs
@@ -0,0 +1,55 @@
+﻿using EducationalPlataform.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EducationalPlataform.Data
+{
+    public class EducationalPlataformContext : DbContext
+    {
+        public EducationalPlataformContext(DbContextOptions<EducationalPlataformContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // relationships 1:N User - Course Create 
+
+            modelBuilder.Entity<Course>()
+                .HasOne(u => u.Creator)
+                .WithMany(c => c.CoursesCreated)
+                .HasForeignKey(c => c.CreatorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // relationships N:N User - Course
+            modelBuilder.Entity<CourseEnrollment>()
+                .HasKey(ce => new { ce.UserId, ce.CourseId });
+
+            modelBuilder.Entity<CourseEnrollment>()
+                .HasOne(ce => ce.User)
+                .WithMany(u => u.CoursesEnrolled)
+                .HasForeignKey(ce => ce.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<CourseEnrollment>()
+                .HasOne(ce => ce.Course)
+                .WithMany(c => c.EnrolledUsers)
+                .HasForeignKey(ce => ce.CourseId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            // Relationship 1:N (Course → Lessons)
+            modelBuilder.Entity<Lesson>()
+                .HasOne(l => l.Course)
+                .WithMany(c => c.Lessons)
+                .HasForeignKey(l => l.CourseId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+
+
+    }
+}
---
+++ Backend/EducationalPlataform/Factories/EducationalPlataformContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using EducationalPlataform.Data;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationalPlataform.Factories
+{
+    public class EducationalPlataformContextFactory : IDesignTimeDbContextFactory<EducationalPlataformContext>
+    {
+        public EducationalPlataformContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<EducationalPlataformContext>();
+            optionsBuilder.UseSqlServer("Server=FABIO\\SQLEXPRESS;Database=EducationalPlataform;Trusted_Connection=True;TrustServerCertificate=True;");
+            return new EducationalPlataformContext(optionsBuilder.Options);
+        }
+    }
+}
