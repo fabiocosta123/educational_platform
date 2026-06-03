37bb5c06268a386bf756cd0e191c12537fd82398
Fabio Costa <developerfabiocsilva@gmail.com>
2026-06-03 16:27:33 -0300

    feat(auth): implement JWT authentication with register and login endpoints

---
+++ Backend/EducationalPlataform/Controllers/AuthController/AuthController.cs
@@ -0,0 +1,93 @@
+﻿using EducationalPlataform.Data;
+using EducationalPlataform.DTOs;
+using EducationalPlataform.Entities;
+using EducationalPlataform.Models.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Text;
+
+namespace EducationalPlataform.Controllers.AuthController
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly EducationalPlataformContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+
+        public AuthController(EducationalPlataformContext context, IConfiguration configuration, IPasswordHasher<User> passwordHasher)
+        {
+            _context = context;
+            _configuration = configuration;
+            _passwordHasher = passwordHasher;
+        }
+
+
+        
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password, UserProfile profile)
+        {
+            if (await _context.Users.AnyAsync(u => u.UserName == username))
+                throw new ArgumentException("Username already exists.");
+
+            var user = new User
+            {
+                UserName = username,
+                Profile = profile
+            };
+
+            user.PasswordHash = _passwordHasher.HashPassword(user, password);
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("User registered successfully.");
+        }
+
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.UserName == username);
+
+            if (user == null)
+                throw new ArgumentException("Invalid credentials");
+
+            var result = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, password);
+
+            if (result == PasswordVerificationResult.Failed)
+                throw new ArgumentException("Invalid credentials");
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.Role, user.Profile.ToString())                    
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new { token = tokenHandler.WriteToken(token) });
+
+        }
+
+        
+
+
+    }
+}
