b5e24ac46483168d4d61198d19f6c09b87930c75
Fabio Costa <developerfabiocsilva@gmail.com>
2026-09-23 20:26:16 -0300

    checkpoint before checking out develop

---
+++ EducationalPlataform.Tests/Controllers/CourseControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using EducationalPlataform.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+
+namespace EducationalPlataform.Tests.Controllers
+{
+    public class CourseControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public CourseControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetById_ReturnsErrorResponse_WhenCourseNotFound()
+        {
+            var response = await _client.GetAsync("/api/courses/9999");
+            var error = await response.Content.ReadFromJsonAsync<ErrorResponse>();
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.NotNull(error);
+            Assert.Equal(400, error.StatusCode);
+            Assert.Equal("Invalid request data: ", error.Message);
+        }
+    }
+}
